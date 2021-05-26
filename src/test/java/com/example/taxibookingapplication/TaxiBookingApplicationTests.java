f30e95c5a1a6caf307e0079e629caf36e8956647
adhikp <prakash.adhikari@reece.com.au>
2021-05-26 15:22:34 +1000

    Created springboot application

---
+++ src/test/java/com/example/taxibookingapplication/TaxiBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxibookingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
