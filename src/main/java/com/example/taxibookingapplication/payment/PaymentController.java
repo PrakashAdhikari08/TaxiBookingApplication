1671607f67c0cd074e83ffd1e94bafa8b52425f6
gaurab546 <gaurabkarki1@gmail.com>
2021-06-27 19:36:39 +1000

    payment api made

--- src/main/java/com/example/taxibookingapplication/payment/PaymentController.java
+++ src/main/java/com/example/taxibookingapplication/payment/PaymentController.java
@@ -9,13 +9,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -25,7 +25,6 @@ public class PaymentController {
     private String paymentServiceUrl;
 
     private RestTemplate restTemplate;
-
     public PaymentController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
@@ -38,13 +37,47 @@ public class PaymentController {
         log.info(paymentServiceUrl);
 
         HttpHeaders headers = new HttpHeaders();
-
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         HttpEntity<Payment> entity = new HttpEntity<>(payment, headers);
 
-        return restTemplate.exchange(paymentServiceUrl+"/payment-register", HttpMethod.POST, entity, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(paymentServiceUrl+"/payment-register",
+                HttpMethod.POST, entity, String.class);
+        return new ResponseEntity(responseEntity.getBody(), responseEntity.getStatusCode());
 
-//        return new ResponseEntity("Card Details Saved", HttpStatus.OK);
+    }
+
+    @PostMapping("/payment/booking/{bookingId}")
+    public ResponseEntity<String> paymentRequest(@RequestBody PaymentRequestDto paymentRequestDto,
+                                                 @PathVariable Integer bookingId){
+        paymentRequestDto.setBookingId(bookingId);
+        paymentRequestDto.getCustomerId();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<PaymentRequestDto> entity = new HttpEntity<>(paymentRequestDto, httpHeaders);
+
+        String url = paymentServiceUrl + "/payment/booking/" + bookingId;
+        log.info(url);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, entity, String.class );
+
+//        if (responseEntity.get)
+//            return new ResponseEntity<>(responseEntity.getBody(), HttpStatus.NOT_FOUND);
+        return  new ResponseEntity(responseEntity.getBody(), responseEntity.getStatusCode());
+    }
+
+    @GetMapping(value = "/payment/getAll/{customerId}")
+    public ResponseEntity<List<PaymentRecord>> getAllPayment(@PathVariable Integer customerId){
+        //        HttpHeaders headers = new HttpHeaders();
+//        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//        HttpEntity <String> entity = new HttpEntity<String>(headers);
+        List<PaymentRecord> paymentRecords = restTemplate.getForObject(paymentServiceUrl + "/payment/getAll/" + customerId, List.class);
+
+        return new ResponseEntity<>(paymentRecords, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<String> handleClientError(){
+        return new ResponseEntity<>("Payment not complete", HttpStatus.NOT_FOUND);
 
     }
 }
